a506555d808dd4fb1320a082461dcafb8b792964
Carlos Fonseca <cardfonseca07@gmail.com>
2026-09-17 13:06:11 -0600

    feat: :sparkles: implement max sum subarray with sliding window

--- src/01-sliding-window/01-max-sum-subarray/max-sum.ts
+++ src/01-sliding-window/01-max-sum-subarray/max-sum.ts
@@ -1,3 +1,48 @@
-export function maxSumSubarray(numbers: number[], windowSize: number): number {
-  return 0;
+export function bruteForce(numbers: number[], windowSize: number): number {
+  let maxSum = 0;
+
+  numbers.forEach((number, index) => {
+    const range = numbers.slice(index, index + windowSize);
+
+    if (range.length < windowSize) {
+      return;
+    }
+
+    const currentSum = range.reduce((acc, num) => acc + num);
+
+    if (index === 0) {
+      maxSum = currentSum;
+      return;
+    }
+
+    maxSum = Math.max(maxSum, currentSum);
+  });
+
+  return maxSum;
+}
+
+export function maxSum(numbers: number[], windowSize: number): number {
+  let result = 0;
+  let currentSum = 0;
+
+  numbers.forEach((_, index) => {
+    if (index + windowSize > numbers.length) {
+      return;
+    }
+
+    const left = numbers[index - 1];
+    const right = numbers[index + windowSize - 1];
+
+    if (index === 0) {
+      currentSum = numbers.slice(0, windowSize).reduce((acc, num) => acc + num);
+      result = currentSum;
+      return;
+    }
+
+    currentSum = currentSum - left + right;
+
+    result = Math.max(result, currentSum);
+  });
+
+  return result;
 }
--- src/01-sliding-window/01-max-sum-subarray/max-sum.unit.test.ts
+++ src/01-sliding-window/01-max-sum-subarray/max-sum.unit.test.ts
@@ -1,15 +1,19 @@
-import { maxSumSubarray } from "./max-sum";
+import { maxSum } from "./max-sum";
 
 describe("maxSumSubarray", () => {
   it("returns the maximum sum of a subarray of size k", () => {
-    expect(maxSumSubarray([2, 1, 5, 1, 3, 2], 3)).toBe(9);
+    expect(maxSum([2, 1, 5, 1, 3, 2], 3)).toBe(9);
   });
 
   it("works when the window contains the entire array", () => {
-    expect(maxSumSubarray([2, 3, 4], 3)).toBe(9);
+    expect(maxSum([2, 3, 4], 3)).toBe(9);
   });
 
   it("works with negative numbers", () => {
-    expect(maxSumSubarray([-2, -1, -5, -1], 2)).toBe(-3);
+    expect(maxSum([-2, -1, -5, -1], 2)).toBe(-3);
+  });
+
+  it("works when the window size is 1", () => {
+    expect(maxSum([2, 5, 1, 3], 1)).toBe(5);
   });
 });
